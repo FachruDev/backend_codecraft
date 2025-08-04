696e374f5a3a15a5aeb6e42fd3d2d093e76edde3
FchDxCode <fachru2006@gmail.com>
2025-08-04 10:41:34 +0700

    refactor: adjust the routes code to the latest permissions

--- src/routes/permissionRoutes.ts
+++ src/routes/permissionRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { getPermissionsController } from '../controller/permissionController';
 import { authenticateToken, requirePermission } from '../middleware/middleware';
-import { Action, AppPermissions, Resources } from '../utils/permission';
+import { AppPermissions } from '../utils/permission';
 
 const router = Router();
 
 router.get('/', 
   authenticateToken, 
-  requirePermission([AppPermissions[Resources.PERMISSION + '_' + Action.VIEW.toUpperCase()]]),
+  requirePermission([AppPermissions.PERMISSION_VIEW]), 
   getPermissionsController
 );
 
